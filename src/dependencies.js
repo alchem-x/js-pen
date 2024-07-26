7e1446a2c60555fa88a8a434cb0922d04597a793
阿尔贝鲁 <cbdyzj@jianzhao.org>
2024-07-26 06:32:14 +0000

    Update deps

--- src/dependencies.js
+++ src/dependencies.js
@@ -1,21 +1,21 @@
 import loadJs from './loadJs.js'
 
 function withCdn(url) {
-    const CDN_BASE = 'https://cdn.staticfile.org/'
+    const CDN_BASE = 'https://unpkg.com/'
     return new URL(url, CDN_BASE).toString()
 }
 
 await Promise.all([
-    loadJs(withCdn('/vue/3.2.31/vue.global.prod.min.js')),
-    loadJs(withCdn('/FileSaver.js/2.0.5/FileSaver.min.js')),
+    loadJs(withCdn('/vue@3.4.34/dist/vue.global.prod.js')),
+    loadJs(withCdn('/file-saver@2.0.5/dist/FileSaver.min.js')),
 ])
 
 async function getMonaco() {
-    await loadJs(withCdn('/monaco-editor/0.33.0/min/vs/loader.js'))
+    await loadJs(withCdn('/monaco-editor@0.50.0/min/vs/loader.js'))
 
     window.require.config({
         paths: {
-            vs: withCdn('/monaco-editor/0.33.0/min/vs'),
+            vs: withCdn('/monaco-editor@0.50.0/min/vs'),
         },
         'vs/nls': {
             availableLanguages: {
